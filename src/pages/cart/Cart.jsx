d83e525a0d34738faa5d746d432b542ec3caccb6
Daya3611 <dayanand.gawade@vsit.edu.in>
2024-12-28 12:14:19 +0530

    cart ui fix

--- src/pages/cart/Cart.jsx
+++ src/pages/cart/Cart.jsx
@@ -165,7 +165,7 @@ function Cart() {
                   </div>
                 </div>
 
-                <div className="mt-4 -ml-5 md:mt-0 flex flex-col md:flex-row items-center justify-between">
+                <div className="mt-4 ml-5 md:mt-0 flex text-center items-center gap-4">
                   <p className="text-xl font-semibold text-gray-700 mr-2">₹{totalItemPrice}</p>
                   
                   {/* Quantity Selector */}
@@ -194,7 +194,7 @@ function Cart() {
                   {/* Remove Item */}
                   <div
                     onClick={() => deleteCart(item)}
-                    className="text-red-600 mt-4 md:mt-0 cursor-pointer flex items-center justify-center"
+                    className="text-red-600  md:mt-0 cursor-pointer flex items-center "
                   >
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                       <path strokeLinecap="round" strokeLinejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
